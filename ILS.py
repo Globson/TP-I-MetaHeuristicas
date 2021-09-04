61aa7b29abf0142ab6ba7d980216a19af5e65692
Globson <samuel.sena@ufv.br>
2021-09-04 09:10:09 -0300

    mod ILS

--- ILS.py
+++ ILS.py
@@ -5,25 +5,24 @@ from HC import entre_limites
 
 #ILS
 
-def hillclimbing(objetivo, limites, iteracoes, step_size, solucao_inicial):
+def hillclimbing(objetivo, limites, iteracoes, Tam_passoHC, solucao_inicial):
 	solucao = solucao_inicial
 	# Calculando valor da solucao inicial
 	valor_solucao = objetivo(solucao)
 	# Modificação
 	for i in range(iteracoes):
-		# take a step
 		candidato = None
 		while candidato is None or not entre_limites(candidato, limites):
-			candidato = solucao + randn(len(limites)) * step_size
+			candidato = solucao + randn(len(limites)) * Tam_passoHC #Tamanho do passo (Pertubação)
 		# Calculando valor de candidato
 		valor_candidato = objetivo(candidato)
 		# Verificando se candidato é melhor
 		if valor_candidato <= valor_solucao:
-			# Armazene novas soluções
+			# Armazenando nova solução melhor
 			solucao, valor_solucao = candidato, valor_candidato
 	return [solucao, valor_solucao]
 
-def ils(objetivo, limites, iteracoes, step_size, reinicios, Tam_P):
+def ils(objetivo, limites, iteracoes, Tam_passoHC, reinicios, Tam_P):
 	# definindo ponto de partida
 	melhor = None
 	li = limites[:,0]
@@ -37,9 +36,9 @@ def ils(objetivo, limites, iteracoes, step_size, reinicios, Tam_P):
 		# gerando um ponto inicial como uma versão perturbada do último melhor
 		ponto_inicial = None
 		while ponto_inicial is None or not entre_limites(ponto_inicial, limites): #verificando se candidato não esta vazio e dentro dos limites
-			ponto_inicial = melhor + randn(len(limites)) * Tam_P
+			ponto_inicial = melhor + randn(len(limites)) * Tam_P #Pertubando 
 		# perform a stochastic hill climbing search
-		solucao, valor = hillclimbing(objetivo, limites, iteracoes, step_size, ponto_inicial)
+		solucao, valor = hillclimbing(objetivo, limites, iteracoes, Tam_passoHC, ponto_inicial)
 		# Verificando se solução encontrada é melhor que melhor atual
 		if valor < valor_melhor:
 			melhor, valor_melhor = solucao, valor
